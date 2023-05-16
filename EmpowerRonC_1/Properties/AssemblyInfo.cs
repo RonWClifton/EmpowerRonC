07112ef974ca56aa3d16140d86e47ab7251dc5da
RonWClifton <133587483+RonWClifton@users.noreply.github.com>
2023-05-16 14:09:20 -0400

    Add project files.

---
+++ EmpowerRonC_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerRonC_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerRonC_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("FEDAA847-7C72-4427-B50D-0D51FBD81987")]
+[assembly: AssemblyVersion("1.0.0.0")]
